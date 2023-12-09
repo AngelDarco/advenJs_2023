0a5d4bd0f59a98fe60ed3a07724a0f7a57bf5d31
Angel Darco <angel92-eme@hotmail.com>
2023-12-09 11:25:43 -0500

    :racehorse: eslint and prettier added

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:sonarjs/recommended",
+  ],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "sonarjs/cognitive-complexity": ["error", 20],
+    "sonarjs/no-identical-functions": "error",
+    "sonarjs/no-identical-expressions": "error",
+  },
+  plugins: ["prettier", "sonarjs"],
+};
